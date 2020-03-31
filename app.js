3feb662bdd9dd98f3ebc8132cf3de1c4a6115ea9
Pranav Pandey <pranavpandey1998official@gmail.com>
2020-03-31 09:21:11 +0530

    updated port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const userRoutes = require("./routes/user");
 const blogRoutes = require('./routes/blog');
